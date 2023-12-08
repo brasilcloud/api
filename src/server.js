df2fd0debd33737327d62f7ba136a9e0e8816771
Vinicius Gabriel <viniciusgabrielpe@gmail.com>
2023-12-08 14:17:13 -0300

    feat(ajuste)

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.use((error, request, response, next) => {
 
 const PORT = process.env.PORTA;
 app.listen(PORT, () => {
-    response.send(`Server is running on Port ${PORT}`);
+    console.log(`Server is running on Port ${PORT}`);
 });
